7d4fc0015c649223c842ab602c9f25fd36f6fdc2
aliumujib <aliuabdulmujib@yahoo.com>
2020-02-24 15:15:49 +0100

    Added screenshots and some more espresso tests

--- mobile-ui/src/androidTest/java/com/aliumujib/countryflags/AllCountriesActivityTest.kt
+++ mobile-ui/src/androidTest/java/com/aliumujib/countryflags/AllCountriesActivityTest.kt
@@ -9,15 +9,27 @@ import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
 import com.aliumujib.countryflags.domain.models.Country
+import com.aliumujib.countryflags.mappers.CountryModelMapper
+import com.aliumujib.countryflags.mappers.CurrencyModelMapper
+import com.aliumujib.countryflags.mappers.LanguageModelMapper
+import com.aliumujib.countryflags.models.CountryModel
+import com.aliumujib.countryflags.presentation.mappers.CountryModelPresentationMapper
+import com.aliumujib.countryflags.presentation.mappers.CurrencyModelPresentationMapper
+import com.aliumujib.countryflags.presentation.mappers.LanguageModelPresentationMapper
 import com.aliumujib.countryflags.ui.AllCountriesActivity
 import com.aliumujib.countryflags.ui.adapters.allcountries.AllCountriesAdapter
+import com.facebook.testing.screenshot.Screenshot
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Maybe
+import kotlinx.android.synthetic.main.fragment_country_details.*
+import kotlinx.android.synthetic.main.search_fragment.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Exception
 
 
 @RunWith(AndroidJUnit4::class)
@@ -28,19 +40,20 @@ class AllCountriesActivityTest {
     var activityTestRule =
         ActivityTestRule<AllCountriesActivity>(AllCountriesActivity::class.java, false, false)
 
+    @JvmField
+    @Rule
+    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     @Test
     fun activityLaunchesTest() {
-        stubCountriesRepositoryGetCountries(Maybe.just(UIDataFactory.makeCountryList(3)))
+        stubCountriesRepositoryGetCountries(Maybe.just(UIDataFactory.makeNonRandomCountryList(3)))
         activityTestRule.launchActivity(null)
     }
 
     @Test
     fun fetchAllCountriesActivityTest() {
-        val countryData = UIDataFactory.makeCountryList(3)
-
+        val countryData = UIDataFactory.makeNonRandomCountryList(3)
         stubCountriesRepositoryGetCountries(Maybe.just(countryData))
-
         activityTestRule.launchActivity(null)
 
         countryData.forEachIndexed { index, country ->
@@ -55,12 +68,13 @@ class AllCountriesActivityTest {
             )
         }
 
+        Screenshot.snapActivity(activityTestRule.activity).record()
     }
 
     @Test
     fun searchAllCountriesActivityTest() {
-        val countryData = UIDataFactory.makeCountryList(3)
-        val searchResults = UIDataFactory.makeCountryList(3)
+        val countryData = UIDataFactory.makeNonRandomCountryList(3)
+        val searchResults = UIDataFactory.makeNonRandomCountryList(3)
 
         stubCountriesRepositoryGetCountries(Maybe.just(countryData))
         stubCountriesRepositorySearchCountries(Maybe.just(searchResults))
@@ -84,6 +98,33 @@ class AllCountriesActivityTest {
             )
         }
 
+        Screenshot.snapActivity(activityTestRule.activity).record()
+    }
+
+
+    @Test
+    fun errorResultsShowErrorStateTest() {
+        val errorMessage = "An error occurred in the back"
+        stubCountriesRepositoryGetCountries(Maybe.error(Throwable(errorMessage)))
+
+        activityTestRule.launchActivity(null)
+
+        onView(withId(R.id.errorView)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.errorView)).check(matches(withText(errorMessage)))
+
+        Screenshot.snapActivity(activityTestRule.activity).record()
+    }
+
+    @Test
+    fun emptyResultsShowEmptyStateTest() {
+
+        stubCountriesRepositoryGetCountries(Maybe.just(emptyList()))
+
+        activityTestRule.launchActivity(null)
+
+        onView(withId(R.id.emptyView)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
+        Screenshot.snapActivity(activityTestRule.activity).record()
     }
 
     private fun stubCountriesRepositorySearchCountries(maybe: Maybe<List<Country>>) {
--- mobile-ui/src/androidTest/java/com/aliumujib/countryflags/runner/TestRunner.kt
+++ mobile-ui/src/androidTest/java/com/aliumujib/countryflags/runner/TestRunner.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.test.runner.AndroidJUnitRunner
 import com.aliumujib.countryflags.TestApplicationClass
+import com.facebook.testing.screenshot.ScreenshotRunner
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
 
@@ -13,6 +14,7 @@ class TestRunner : AndroidJUnitRunner() {
 
     override fun onCreate(arguments: Bundle) {
         super.onCreate(arguments)
+        ScreenshotRunner.onCreate(this, arguments)
         RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
         RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
         RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
@@ -22,5 +24,10 @@ class TestRunner : AndroidJUnitRunner() {
     @Throws(InstantiationException::class, IllegalAccessException::class, ClassNotFoundException::class)
     override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
         return super.newApplication(cl, TestApplicationClass::class.java.name, context)
+    }
+
+    override fun finish(resultCode: Int, results: Bundle?) {
+        ScreenshotRunner.onDestroy()
+        super.finish(resultCode, results)
     }
 }
